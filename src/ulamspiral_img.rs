4a622b969db7dbe907e8f29bb102e0a42d3a52a2
Ben Brook <hellobenbrook@protonmail.com>
2022-02-27 13:23:25 +0000

    Optimise img generate

--- src/ulamspiral_img.rs
+++ src/ulamspiral_img.rs
@@ -1,29 +1,30 @@
-pub fn generate(x_size: u32, y_size: u32) -> image::ImageBuffer<image::Luma<u8>, Vec<u8>> {
-    if x_size == 0 || y_size == 0 {
-        return image::ImageBuffer::new(0, 0);
-    }
-    // assert!(x_size > 0 && y_size > 0, "inputs must be greater than 0");
+use std::{cmp, error::Error};
 
+pub fn generate(
+    x_size: u32,
+    y_size: u32,
+) -> Result<image::ImageBuffer<image::Luma<u8>, Vec<u8>>, Box<dyn Error>> {
     let mut img = image::ImageBuffer::new(x_size, y_size);
-    let x_size = x_size as i32;
-    let y_size = y_size as i32;
+    let total = u64::from(cmp::max(x_size, y_size)).pow(2).try_into()?;
+    let sieve = primal::Sieve::new(total);
 
-    for x in (0..x_size).map(|x| x - x_size / 2) {
-        for y in (0..y_size).map(|y| y - y_size / 2) {
-            let coord = crate::Coord::new(x, y);
-            let deets = crate::get_ulam_deets(&coord);
-            let value = deets.value;
-            if primal::is_prime(value as u64) {
-                let pixel = image::Luma::from([255 as u8]);
-                img.put_pixel(
-                    (x_size / 2 + x) as u32,
-                    ((y_size - y_size / 2) - y - 1) as u32,
-                    pixel,
-                );
-            }
+    for prime in sieve.primes_from(0).take_while(|x| *x <= total) {
+        let coord = crate::lookup::lookup(prime.try_into()?);
+        let pos_x: i64 = i64::from(x_size) - i64::from(x_size) / 2 + i64::from(coord.x);
+        let pos_y: i64 = (i64::from(y_size) / 2 - i64::from(coord.y)) + 1;
+        if pos_x < 1
+            || u32::try_from(pos_x)? > x_size
+            || pos_y < 1
+            || u32::try_from(pos_y)? > y_size
+        {
+            // A rectangle is being generated and this prime lies outside it.
+            continue;
         }
+        let pixel = image::Luma::from([255]);
+        img.put_pixel(u32::try_from(pos_x)? - 1, u32::try_from(pos_y)? - 1, pixel)
     }
-    img
+
+    Ok(img)
 }
 
 #[cfg(test)]
@@ -31,7 +32,7 @@ mod tests {
     use super::*;
     #[test]
     fn check_sq() {
-        let odd = generate(5, 5);
+        let odd = generate(5, 5).unwrap();
         let mut expected_odd = image::ImageBuffer::new(5, 5);
         expected_odd.put_pixel(3, 0, image::Luma::from([255 as u8]));
         expected_odd.put_pixel(0, 1, image::Luma::from([255 as u8]));
@@ -44,26 +45,25 @@ mod tests {
         expected_odd.put_pixel(3, 4, image::Luma::from([255 as u8]));
         assert_eq!(odd, expected_odd);
 
-        let even = generate(4, 4);
+        let even = generate(4, 4).unwrap();
         let mut expected_even = image::ImageBuffer::new(4, 4);
-        expected_even.put_pixel(0, 0, image::Luma::from([255 as u8]));
         expected_even.put_pixel(2, 0, image::Luma::from([255 as u8]));
-        expected_even.put_pixel(3, 0, image::Luma::from([255 as u8]));
         expected_even.put_pixel(1, 1, image::Luma::from([255 as u8]));
+        expected_even.put_pixel(2, 1, image::Luma::from([255 as u8]));
+        expected_even.put_pixel(3, 1, image::Luma::from([255 as u8]));
         expected_even.put_pixel(0, 2, image::Luma::from([255 as u8]));
-        expected_even.put_pixel(2, 2, image::Luma::from([255 as u8]));
-        expected_even.put_pixel(3, 3, image::Luma::from([255 as u8]));
+        expected_even.put_pixel(1, 3, image::Luma::from([255 as u8]));
         assert_eq!(even, expected_even);
     }
     #[test]
     fn check_bad() {
-        let bad = generate(0, 1);
-        let img = image::ImageBuffer::new(0, 0);
-        assert_eq!(bad, img);
+        let bad = generate(0, 1).unwrap();
+        let expected = image::ImageBuffer::new(0, 1);
+        assert_eq!(bad, expected);
     }
     #[test]
     fn check_rect() {
-        let tall = generate(5, 3);
+        let tall = generate(5, 3).unwrap();
         let mut expected_tall = image::ImageBuffer::new(5, 3);
         expected_tall.put_pixel(0, 0, image::Luma::from([255 as u8]));
         expected_tall.put_pixel(2, 0, image::Luma::from([255 as u8]));
@@ -74,7 +74,7 @@ mod tests {
         expected_tall.put_pixel(2, 2, image::Luma::from([255 as u8]));
         assert_eq!(tall, expected_tall);
 
-        let wide = generate(3, 5);
+        let wide = generate(3, 5).unwrap();
         let mut expected_wide = image::ImageBuffer::new(3, 5);
         expected_wide.put_pixel(2, 0, image::Luma::from([255 as u8]));
         expected_wide.put_pixel(1, 1, image::Luma::from([255 as u8]));
