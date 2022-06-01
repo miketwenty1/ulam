ee24619adfbb675a243148dfc6d1f35905736b38
Michael Tidwell <tidwell@zebedee.io>
2022-05-31 23:24:56 -0400

    commit colors

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # ulam ftw 
 takes a Coord( x, y )
+
+```rust
+    let mut ulam_points = HashMap::new();
+
+    for x in -201..201 {
+        if x % 10 == 0 {
+            println!("{x}");
+        }
+        for y in -201..201 {
+            let c = Coord::new(x, y);
+            let a = ulam::get_ulam_deets(&c);
+
+            ulam_points.insert(
+                a.value,
+                c
+            );
+        }
+    }
+
+
+    let c = Coord::new(20000, 20000);
+    let a = ulam::get_ulam_deets(&c);
+
+    println!("{}", a.value);
+    // println!("{:?}", ulam_points[&55]);
+
+
+
+    ulam::ulamspiral_img::generate(1_000, 1_000)
+        .unwrap()
+        .save("./result.png");
+```
--- src/ulamspiral_img.rs
+++ src/ulamspiral_img.rs
@@ -1,9 +1,11 @@
 use std::{cmp, error::Error};
 
+pub type GreyImage = image::GrayImage; // because ben is british
+
 pub fn generate(
     x_size: u32,
     y_size: u32,
-) -> Result<image::ImageBuffer<image::Luma<u8>, Vec<u8>>, Box<dyn Error>> {
+) -> Result<GreyImage, Box<dyn Error>> {
     let mut img = image::ImageBuffer::new(x_size, y_size);
     let total = u64::from(cmp::max(x_size, y_size)).pow(2).try_into()?;
     let sieve = primal::Sieve::new(total);
@@ -21,6 +23,45 @@ pub fn generate(
             // A rectangle is being generated and this prime lies outside it.
             continue;
         }
+        img.put_pixel(u32::try_from(pos_x)? - 1, u32::try_from(pos_y)? - 1, pixel);
+    }
+
+    Ok(img)
+}
+
+const RED: [u8; 3] = [255, 0, 0];
+const GREEN: [u8; 3] = [0, 255, 0];
+const BLUE: [u8; 3] = [0, 0, 255];
+
+pub fn generate_colour( // because Ben is british color is spelled colour.
+    x_size: u32,
+    y_size: u32,
+) -> Result<image::RgbImage, Box<dyn Error>> {
+    let mut img = image::RgbImage::new(x_size, y_size);
+    let total = u64::from(cmp::max(x_size, y_size)).pow(2).try_into()?;
+    let sieve = primal::Sieve::new(total);
+    
+    for prime in sieve.primes_from(0).take_while(|x| *x <= total) {
+        let coord = crate::calc_coord::calc(prime.try_into()?);
+        let pos_x: i64 = i64::from(x_size) - i64::from(x_size) / 2 + i64::from(coord.x);
+        let pos_y: i64 = i64::from(y_size) / 2 - i64::from(coord.y) + 1;
+        if pos_x < 1
+            || u32::try_from(pos_x)? > x_size
+            || pos_y < 1
+            || u32::try_from(pos_y)? > y_size
+        {
+            // A rectangle is being generated and this prime lies outside it.
+            continue;
+        }
+        
+        let pixel = image::Rgb(match prime % 6 {
+            0 => [255, 0, 0],
+            1 => [0, 255, 0],
+            3 => [255, 0, 0],
+            5 => [0, 0, 255],
+            _ => [255, 255, 255]
+        });
+
         img.put_pixel(u32::try_from(pos_x)? - 1, u32::try_from(pos_y)? - 1, pixel);
     }
 
@@ -60,6 +101,11 @@ mod tests {
         let bad = generate(0, 1).unwrap();
         let expected = image::ImageBuffer::new(0, 1);
         assert_eq!(bad, expected);
+    }
+    #[test]
+    #[ignore] 
+    fn try_generate() {
+        generate_colour(2_000, 2_000).unwrap().save("./result.png").unwrap();
     }
     #[test]
     fn check_rect() {
